02865619d7bad0c8732275203eabc0e1186a74dc
Leonel Lopez <leonellopez647@gmail.com>
2021-07-18 09:59:44 -0600

    Bug al correr run watch no mostrando la imagen

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,7 +28,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/home') }}">
-                    <img src="{{ mix('imgs/logo.png')}}" height="40px" width="160px">
+                    <img src="{{ asset('imgs/logo.png')}}" height="40px" width="160px">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse"
                     data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
